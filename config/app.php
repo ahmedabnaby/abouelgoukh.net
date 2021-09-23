22cc346aeebc633b21a657d906a03f38b4321516
Mizo <ahmedabnaby.97@gmail.com>
2021-09-23 19:14:51 +0200

    Removed unwanted files and folders

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Darryldecode\Cart\CartServiceProvider::class,
-        BaklySystems\PayMob\PayMobServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -231,7 +230,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Cart' => Darryldecode\Cart\Facades\CartFacade::class,
-        'PayMob' => BaklySystems\PayMob\Facades\PayMob::class,
     ],
 
 ];
